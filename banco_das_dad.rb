fe26bb36923b7ee9cb9b28b92eae01b376c86b44
leomilrib <leomilrib@gmail.com>
2013-07-30 19:59:44 -0300

    commit da solução em ruby

---
+++ banco_das_dad.rb
@@ -0,0 +1,47 @@
+=begin
+  cada caixa é um objeto que guarda (tempo_cliente, tempo_atend);
+  a definição se o cliente pega fila ou não é dado pelo seu horário de chegada:
+    - se seu horário de chegada for menor do que o menor tempo_atend, sua espera é igual ao tempo_cliente do menor tempo_atend menos seu horário de chegada.
+    - caso contrário, não há espera.
+=end
+
+require './caixa.rb'
+
+def main 
+  entrada = File.readlines("./entrada")
+  fila(entrada)
+end
+
+def fila entrada
+  caixas_abertos = []
+  atendimentos_atrasados = 0
+
+  c = entrada[0].split(' ').first.to_i
+  n = entrada[0].split(' ').last.delete('\n').to_i
+
+  for i in (0...c)
+    caixas_abertos.push Caixa.new(entrada[i+1])
+  end
+
+  for i in (c+1...n)
+    espera = proximo_caixa(caixas_abertos).atender_cliente(entrada[i])
+
+    if espera > 20
+      atendimentos_atrasados +=1
+    end
+  end
+
+  puts atendimentos_atrasados
+end
+
+def proximo_caixa caixas
+  caixa = caixas[0]
+  caixas.each do |cx|
+    if cx.tempo_atend < caixa.tempo_atend
+      caixa = cx
+    end
+  end
+  caixa
+end
+
+main
